7785ffa2483235534c908e70c12b0ab8201f62a4
Volf5994 <149259085+Volf5994@users.noreply.github.com>
2026-04-28 07:02:45 +0300

    erfe

--- Выполненные задания/WF_2_1_sr/WF_2_1_sr/Form1.cs
+++ Выполненные задания/WF_2_1_sr/WF_2_1_sr/Form1.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace WF_2_1_sr
 {
 	public partial class Form1 : Form
 	{
+		private List<Point> circles = new List<Point>();
+		private const int Radius = 30;
 		public Form1()
 		{
 			InitializeComponent();
+			this.DoubleBuffered = true;
+			this.MouseClick += (s, e) =>
+			{
+				if (e.Button == MouseButtons.Left)
+				{
+					circles.Add(e.Location);
+					this.Invalidate();
+				}
+			};
+			this.Paint += (s, e) =>
+			{
+				e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+				foreach (Point p in circles)
+				{
+					e.Graphics.FillEllipse(Brushes.Red, p.X - Radius, p.Y - Radius, Radius * 2, Radius * 2);
+					e.Graphics.DrawEllipse(Pens.Black, p.X - Radius, p.Y - Radius, Radius * 2, Radius * 2);
+				}
+			};
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
 		}
 	}
 }
